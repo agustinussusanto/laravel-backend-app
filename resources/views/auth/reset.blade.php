7c43915cf460d2bb46131df8246aa42ebf90c65f
Agustinus <49600251+agustinussusanto@users.noreply.github.com>
2023-12-19 22:14:51 +0700

    first commit

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -14,48 +14,45 @@
 
         <div class="card-body">
             <p class="text-muted">We will send a link to reset your password</p>
-            <form method="POST">
+            <form method="POST" action="{{ route('password.update') }}">
+                @csrf
                 <div class="form-group">
+                    <input type="text" name="token" value="{{ $request->token }}" hidden>
                     <label for="email">Email</label>
-                    <input id="email"
-                        type="email"
-                        class="form-control"
-                        name="email"
-                        tabindex="1"
-                        required
-                        autofocus>
+                    <input id="email" type="email"
+                        class="form-control @error('email')
+                        is-invalid
+                    @enderror"
+                        name="email" tabindex="1" autofocus value="{{ $request->email }}" readonly>
+                    @error('email')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="form-group">
                     <label for="password">New Password</label>
-                    <input id="password"
-                        type="password"
-                        class="form-control pwstrength"
-                        data-indicator="pwindicator"
-                        name="password"
-                        tabindex="2"
-                        required>
-                    <div id="pwindicator"
-                        class="pwindicator">
-                        <div class="bar"></div>
-                        <div class="label"></div>
-                    </div>
+                    <input id="password" type="password"
+                        class="form-control @error('password')
+                    is-invalid
+                    @enderror"
+                        name="password" tabindex="2">
+                    @error('password')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="form-group">
                     <label for="password-confirm">Confirm Password</label>
-                    <input id="password-confirm"
-                        type="password"
-                        class="form-control"
-                        name="confirm-password"
-                        tabindex="2"
-                        required>
+                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
+                        tabindex="2">
                 </div>
 
                 <div class="form-group">
-                    <button type="submit"
-                        class="btn btn-primary btn-lg btn-block"
-                        tabindex="4">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
                         Reset Password
                     </button>
                 </div>
